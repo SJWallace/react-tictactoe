2d1cef286a6e1d9779d9385b72b2d2a384ac06c2
fireslide <fireslide@gmail.com>
2021-01-13 15:35:55 +1030

    handleClick function added, board state contains data

--- src/index.js
+++ src/index.js
@@ -13,18 +13,31 @@ class Square extends React.Component {
     }
     render() {
         return (
-            <button className="square"
-                    onClick={() => this.setState({value: 'X'})}
+            <button
+                className="square"
+                onClick={() => this.props.onClick()}
             >
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
@@ -50,6 +63,12 @@ class Board extends React.Component {
                 </div>
             </div>
         );
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
     }
 }
 
